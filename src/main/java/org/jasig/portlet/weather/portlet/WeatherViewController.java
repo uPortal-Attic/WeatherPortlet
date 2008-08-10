aa5a0baa9842401708e9c0a2e4adef8848dc2022
Dustin Schultz <dustin.schultz@yale.edu>
2008-08-10 00:22:24 +0000

    WPT-2 Refactored annotated controller again.
    git-svn-id: https://source.jasig.org/sandbox/WeatherPortlet/branches/WeatherPortlet-Spring25@16143 f5dbab47-78f9-eb45-b975-e544023573eb

--- src/main/java/org/jasig/portlet/weather/portlet/WeatherViewController.java
+++ src/main/java/org/jasig/portlet/weather/portlet/WeatherViewController.java
@@ -1,15 +1,12 @@
 package org.jasig.portlet.weather.portlet;
 
-import java.util.Collection;
-
 import javax.portlet.PortletPreferences;
 import javax.portlet.RenderRequest;
 
-import org.jasig.portlet.weather.domain.Weather;
 import org.jasig.portlet.weather.service.IWeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,16 +16,10 @@ public class WeatherViewController {
 	@Autowired
 	private IWeatherService weatherService;
 	
-	private static final String VIEW_NAME = "viewWeather";
-	
-	@ModelAttribute("weathers")
-	public Collection<Weather> getAllWeathers(RenderRequest request) {
-		PortletPreferences preferences = request.getPreferences();
-		return weatherService.getAllWeather(preferences);
-	}
-	
 	@RequestMapping
-	public String getView() {
-		return VIEW_NAME;
+	public String displayAllWeather(RenderRequest request, Model model) {
+		PortletPreferences preferences = request.getPreferences();
+		model.addAttribute("weathers", weatherService.getAllWeather(preferences));
+		return "viewWeather";
 	}
 }
