c4e2690c55852d3cc682d71b9c43d992208b04c7
Jen Bourey <jennifer.bourey@gmail.com>
2011-12-21 19:30:42 +0000

    WPT-51 Adding jQuery Mobile view for main view and a view selector aimed at uPortal 4.
    git-svn-id: https://source.jasig.org/portlets/WeatherPortlet/trunk@25601 f5dbab47-78f9-eb45-b975-e544023573eb

---
+++ src/main/java/org/jasig/portlet/weather/portlet/ThemeNameViewSelectorImpl.java
@@ -0,0 +1,61 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.jasig.portlet.weather.portlet;
+
+import javax.portlet.PortletRequest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ThemeNameViewSelectorImpl implements IViewSelector {
+    
+    protected static final String THEME_NAME_PROPERTY = "themeName";
+    protected static final String MOBILE_THEMES_KEY = "mobileThemes";
+    protected static final String[] MOBILE_THEMES_DEFAULT = new String[]{ "UniversalityMobile" };
+
+    /*
+     * (non-Javadoc)
+     * @see org.jasig.portlet.calendar.mvc.IViewSelector#getCalendarViewName(javax.portlet.PortletRequest)
+     */
+    public String getViewNameExtension(PortletRequest request) {
+        if (isMobile(request)) {
+            return "-jQM";
+        } else {
+            return "";
+        }
+    }
+    
+    protected boolean isMobile(PortletRequest request) {
+        String[] mobileThemes = request.getPreferences().getValues(MOBILE_THEMES_KEY, MOBILE_THEMES_DEFAULT);
+        String themeName = request.getProperty(THEME_NAME_PROPERTY);
+        if (themeName == null) {
+            return false;
+        }
+        
+        for (String theme : mobileThemes) {
+            if (themeName.equals(theme)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+}
